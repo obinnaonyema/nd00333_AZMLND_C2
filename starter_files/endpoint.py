11727e0b6e49452832dc77281cd3d493864f40f5
Obinna Onyema <53495609+obinnaonyema@users.noreply.github.com>
2021-02-20 23:58:26 +0100

    Update endpoint.py

--- starter_files/endpoint.py
+++ starter_files/endpoint.py
@@ -3,7 +3,7 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ''
+scoring_uri = ''#use yours
 # If the service is authenticated, set the key or token
 key = ''
 
